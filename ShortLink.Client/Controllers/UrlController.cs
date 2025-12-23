9d1aef59a7f82e347fb6c918535928737a93c0bf
priyanka.rambhad@eliteinsurance.in <priyanka.rambhad@eliteinsurance.in>
2025-12-23 17:16:56 +0530

    Adding new Controller - UrlController

---
+++ ShortLink.Client/Controllers/UrlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShortLink.Client.Controllers
+{
+    public class UrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
