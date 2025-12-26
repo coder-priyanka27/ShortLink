07bd531f650972c46f86ad3162435732bd6d6a4f
priyanka.rambhad@eliteinsurance.in <priyanka.rambhad@eliteinsurance.in>
2025-12-26 14:11:24 +0530

    Add First Model

--- ShortLink.Client/Controllers/HomeController.cs
+++ ShortLink.Client/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using ShortLink.Client.Models;
 
 namespace ShortLink.Client.Controllers
 {
@@ -23,10 +22,5 @@ namespace ShortLink.Client.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- ShortLink.Client/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShortLink.Client.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
