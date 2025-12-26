ccd7fba803752eb974a4f60b131eca8a7a66405f
priyanka.rambhad@eliteinsurance.in <priyanka.rambhad@eliteinsurance.in>
2025-12-26 18:32:34 +0530

    Passing Data from Controller to View.

--- ShortLink.Client/Controllers/UrlController.cs
+++ ShortLink.Client/Controllers/UrlController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShortLink.Client.Data.Models;
 
 namespace ShortLink.Client.Controllers
 {
@@ -6,7 +7,43 @@ namespace ShortLink.Client.Controllers
     {
         public IActionResult Index()
         {
+            //// Data is from DB
+            //var urlDb = new Url()
+            //{
+            //    Id = 1,
+            //    OriginalLink = "https://original.com",
+            //    ShortLink = "Shortlink",
+            //    NoOfClicks = 1,
+            //    UserId = 1,
+            //};
+
+            //var allData = new List<Url>();
+            //allData.Add(urlDb);
+            //ViewData["ShortenedUrl"] = "This is just a short url";
+            //ViewData["AllUrls"] = new List<string>() { "Url 1", "Url 2", "Url 3" };
+            //ViewBag.ShortenedUrl = "This is just a short url";
+            //ViewBag.AllUrls = new List<string>() { "Url 1", "Url 2", "Url 3", "Url 4" };
+            var tempData = TempData["Successmessage"];
+            var viewBag = ViewBag.Test1;
+            var viewData = ViewData["Test2"];
+            if (TempData["Successmessage"] != null)
+            {
+               if(TempData["Successmessage"] != null)
+                {
+                    ViewBag.Successmessage = TempData["Successmessage"].ToString();
+                }
+            }
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            //Shorten Url
+            var shortenUrl = "Short";
+            TempData["Successmessage"] = "Sucessful!";
+            ViewBag.Test1 = "Test1";
+            ViewData["Test2"] = "Test2";
+            return RedirectToAction("Index");
         }
     }
 }
