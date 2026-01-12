3c3e855499151784f43c98ebb9f0af51eed7609b
coder-priyanka27 <priyankarambhad09@gmail.com>
2026-01-12 18:31:29 +0530

    Creating the DbContext file

---
+++ ShortLink.Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShortLink.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLink.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Url> Urls { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
