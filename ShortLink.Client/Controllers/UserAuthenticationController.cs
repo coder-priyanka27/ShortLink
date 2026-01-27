0a883069d49d3181314dfe625a39ed9b571f76c7
coder-priyanka27 <priyankarambhad09@gmail.com>
2026-01-27 16:31:48 +0530

    Signing users using SignInManager

--- ShortLink.Client/Controllers/UserAuthenticationController.cs
+++ ShortLink.Client/Controllers/UserAuthenticationController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShortLink.Client.Data.ViewModels;
 using ShortLink.Data;
+using ShortLink.Data.Models;
 using ShortLink.Data.Services;
 
 namespace ShortLink.Client.Controllers
@@ -9,9 +11,13 @@ namespace ShortLink.Client.Controllers
     public class UserAuthenticationController : Controller
     {
         private IUsersService _usersService;
-        public UserAuthenticationController(IUsersService usersService)
-        {
+        private SignInManager<AppUser> _signInManager;
+        private UserManager<AppUser> _userManager;
+        public UserAuthenticationController(IUsersService usersService, SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        { 
             _usersService = usersService;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
         public async Task<IActionResult> Users()
         {
@@ -28,6 +34,27 @@ namespace ShortLink.Client.Controllers
             if(!ModelState.IsValid)
             {
                 return View("Login", loginViewModel);
+            }
+
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+            if(user != null)
+            {
+                var userPasswordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if(userPasswordCheck)
+                {
+                    var userLoggedIn = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+
+                    if(userLoggedIn.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid login attempt. Please, check your username and password");
+                        return View("Login", loginViewModel);
+                    }
+                }
             }
             return RedirectToAction("Index", "Home");
         }
